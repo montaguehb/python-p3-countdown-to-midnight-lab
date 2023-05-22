34a15c488ab2ff13edf66fce41b51947961a42c7
Historia Montague <historia.montague@outlook.com>
2023-05-22 14:01:25 -0600

    Finished Lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,19 @@
 # your code goes here!
+import time
+
+def countdown(wait):
+    x = wait
+    while x > 0:
+        print(f"{x} SECOND(S)!")
+        x -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(wait):
+    x = wait
+    while x > 0:
+        print(f"{x} SECOND(S)!")
+        x -= 1
+        time.sleep(1)
+    print("HAPPY NEW YEAR!")
+
+countdown(5)
